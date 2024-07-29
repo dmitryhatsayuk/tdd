c99451795457e90b8aa65bbf73a9c168fb765c63
dmitryhatsayuk <155708514+dmitryhatsayuk@users.noreply.github.com>
2024-07-29 23:21:18 +0700

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class PhoneBook {
+    private static HashMap<String , Integer> book;
     public int add (String name,int number){
-        return 0;
+        if (book==null){
+            book = new HashMap<>();
+        }
+        book.put(name,number);
+        return number;
     }
 }
